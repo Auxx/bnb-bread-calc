e8cb950ce6e93f89ff09a20dad3284afbbb7953d
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2020-08-15 17:10:50 +0100

    Updated tests

--- projects/lib/src/lib/preferment-stage/preferment-stage.spec.ts
+++ projects/lib/src/lib/preferment-stage/preferment-stage.spec.ts
@@ -10,7 +10,7 @@ import { PrefermentFormula } from './preferment-stage.types';
 describe('ppreferment-stage.ts', () => {
   describe('translatePrefermentPercentages', () => {
     it('should translate percentage values to ratios', () => {
-      const result = translatePrefermentPercentages({ starterHydration: 125, outputHydration: 80, flourRatio: 9, prefermentedAmount: 1 });
+      const result = translatePrefermentPercentages({ starterHydration: 125, outputHydration: 80, flourRatio: 9 });
 
       expect(result.starterHydration).toBe(1.25);
       expect(result.outputHydration).toBe(0.8);
@@ -20,7 +20,7 @@ describe('ppreferment-stage.ts', () => {
 
   describe('translatePrefermentRatios', () => {
     it('should translate ratios values to percentages', () => {
-      const result = translatePrefermentRatios({ starterHydration: 1.25, outputHydration: 0.8, flourRatio: 9, prefermentedAmount: 1 });
+      const result = translatePrefermentRatios({ starterHydration: 1.25, outputHydration: 0.8, flourRatio: 9 });
 
       expect(result.starterHydration).toBe(125);
       expect(result.outputHydration).toBe(80);
@@ -30,13 +30,13 @@ describe('ppreferment-stage.ts', () => {
 
   describe('calculatePrefermentWeights', () => {
     it('should calculate weights correctly', () => {
-      let result = calculatePrefermentWeights({ starterHydration: 1, outputHydration: 1, flourRatio: 2, prefermentedAmount: 1 }, 90);
+      let result = calculatePrefermentWeights({ starterHydration: 1, outputHydration: 1, flourRatio: 2 }, 90);
 
       expect(result.starter).toBe(30);
       expect(result.flour).toBe(30);
       expect(result.water).toBe(30);
 
-      result = calculatePrefermentWeights({ starterHydration: 1.5, outputHydration: 1, flourRatio: 5, prefermentedAmount: 1 }, 120);
+      result = calculatePrefermentWeights({ starterHydration: 1.5, outputHydration: 1, flourRatio: 5 }, 120);
 
       expect(result.starter).toBe(25);
       expect(result.flour).toBe(50);
