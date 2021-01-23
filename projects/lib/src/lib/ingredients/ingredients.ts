e8e7f878569c0e2b907b57d4a40c82c8c8acaea8
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-01-23 15:08:00 +0000

    Added default ingredient list

---
+++ projects/lib/src/lib/ingredients/ingredients.ts
@@ -0,0 +1,65 @@
+export interface Ingredient {
+  id: string;
+  type: IngredientType;
+
+  name?: string;
+  meta?: string;
+  description?: string;
+}
+
+export enum IngredientType {
+  flour = 'flour',
+  hydration = 'hydration',
+  other = 'other',
+  combined = 'combined' // TODO Add support
+}
+
+export const ingredients: Ingredient[] = [
+  // Wheat flour
+  { id: 'W405', type: IngredientType.flour, name: 'Pastry flour', meta: 'Wheat' },
+  { id: 'W550', type: IngredientType.flour, name: 'Plain flour', meta: 'Wheat' },
+  { id: 'W700', type: IngredientType.flour, name: 'Bread flour', meta: 'Wheat' },
+  { id: 'W812', type: IngredientType.flour, meta: 'Wheat' },
+  { id: 'W1050', type: IngredientType.flour, meta: 'Wheat' },
+  { id: 'W1200', type: IngredientType.flour, meta: 'Wheat' },
+  { id: 'W1600', type: IngredientType.flour, meta: 'Wheat' },
+  { id: 'W1800', type: IngredientType.flour, name: 'Whole grain flour', meta: 'Wheat' },
+
+  // Rye flour
+  { id: 'R610', type: IngredientType.flour, name: 'Pastry rye flour', meta: 'Rye' },
+  { id: 'R815', type: IngredientType.flour, name: 'White rye flour', meta: 'Rye' },
+  { id: 'R997', type: IngredientType.flour, name: 'Light rye flour', meta: 'Rye' },
+  { id: 'R1150', type: IngredientType.flour, meta: 'Rye' },
+  { id: 'R1370', type: IngredientType.flour, name: 'Medium rye flour', meta: 'Rye' },
+  { id: 'R1740', type: IngredientType.flour, name: 'Dark rye flour', meta: 'Rye' },
+  { id: 'R2000', type: IngredientType.flour, name: 'Whole grain rye flour', meta: 'Rye' },
+  { id: 'R2500', type: IngredientType.flour, name: 'Bran enriched rye flour', meta: 'Rye' },
+
+  // Malts
+  { id: 'raw-rye-malt', type: IngredientType.flour, name: 'Raw rye malt', meta: 'Rye' },
+  { id: 'roasted-rye-malt', type: IngredientType.flour, name: 'Roasted rye malt', meta: 'Rye' },
+  { id: 'fermented-rye-malt', type: IngredientType.flour, name: 'Fermented rye malt', meta: 'Rye' },
+  { id: 'diastatic-malt', type: IngredientType.flour, name: 'Diastatic malt', meta: 'Barley' },
+
+  // Hydration
+  { id: 'water', type: IngredientType.hydration, name: 'Water' },
+  { id: 'beer', type: IngredientType.hydration, name: 'Beer' },
+  { id: 'cider', type: IngredientType.hydration, name: 'Cider' },
+  { id: 'vinegar', type: IngredientType.hydration, name: 'Vinegar' },
+
+  // Other
+  { id: 'salt', type: IngredientType.other, name: 'Salt' },
+  { id: 'caraway', type: IngredientType.other, name: 'Caraway seeds' },
+  { id: 'cumin', type: IngredientType.other, name: 'Cumin seeds' },
+  { id: 'fennel', type: IngredientType.other, name: 'Fennel seeds' },
+  { id: 'anise', type: IngredientType.other, name: 'Anise seeds' },
+  { id: 'coriander', type: IngredientType.other, name: 'Coriander seeds' },
+  { id: 'caraway', type: IngredientType.other, name: 'Caraway seeds' },
+  { id: 'caraway', type: IngredientType.other, name: 'Caraway seeds' },
+  { id: 'pumpkin-seeds', type: IngredientType.other, name: 'Pumpkin seeds' },
+  { id: 'sunflower-seeds', type: IngredientType.other, name: 'Sunflower seeds' },
+  { id: 'oil', type: IngredientType.other, name: 'Oil', meta: 'Any' },
+  { id: 'veg-oil', type: IngredientType.other, name: 'Vegetable oil' },
+  { id: 'olive-oil', type: IngredientType.other, name: 'Olive oil' },
+  { id: 'butter', type: IngredientType.other, name: 'Butter' }
+];
--- projects/lib/src/public-api.ts
+++ projects/lib/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of lib
  */
 
+export * from './lib/ingredients/ingredients';
+
+export * from './lib/inline-parser/inline-parser';
+export * from './lib/inline-parser/inline-parser.types';
+
 export * from './lib/preferment-stage/preferment-stage';
 export * from './lib/preferment-stage/preferment-stage-builder';
 export * from './lib/preferment-stage/preferment-stage.types';
