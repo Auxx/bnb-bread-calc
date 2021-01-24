12ee0e5059c39bf97509b3b174b07f1554505b26
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-01-24 12:15:45 +0000

    Added sample printer

---
+++ print.js
@@ -0,0 +1,30 @@
+const { printToTerminal, baseIngredients } = require('./dist/lib');
+
+const formula = `
+  type:preferment
+  id:sourdough
+  flourType:W1800
+  prefermented:10%
+  flourRatio:2
+  starterHydration:100%
+  outputHydration:80%
+  ---
+  type:simple
+  id:scald
+  W1800:30%
+  water:65%:sourdough
+  raw-rye-malt:3%
+  ---
+  type:simple
+  id:sour-scald
+  stage:sourdough
+  stage:scald
+  ---
+  type:simple
+  id:dough
+  stage:sour-scald
+  W1800:57%
+  salt:2%
+`;
+
+printToTerminal(formula, 800, true, baseIngredients);
