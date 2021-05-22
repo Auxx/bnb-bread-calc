09083cbd228e38f86fe2b1c5288da7c2b983e7ea
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-05-22 16:14:12 +0100

    Added more samples to test out printer

--- print.js
+++ print.js
@@ -32,7 +32,7 @@ const bunFormula = `
   id:sourdough
   flourType:W550
   prefermented:50%
-  flourRatio:5
+  flourRatio:7
   starterHydration:100%
   outputHydration:70%
   ---
@@ -51,5 +51,42 @@ const bunFormula = `
   sugar:15%
 `;
 
-printToCsv(bunFormula, 800, true, baseIngredients);
-// printToTerminal(bunFormula, 630, true, baseIngredients);
+const pizzaFormula = `
+  type:preferment
+  id:sourdough
+  flourType:W700
+  prefermented:40%
+  flourRatio:7
+  starterHydration:100%
+  outputHydration:80%
+  ---
+  type:simple
+  id:dough
+  stage:sourdough
+  W700:100%:sourdough
+  water:75%:sourdough
+  olive-oil: 2%
+  salt:2%
+`;
+
+const wwRyeFormula = `
+  type:preferment
+  id:sourdough
+  flourType:R2000
+  prefermented:40%
+  flourRatio:7
+  starterHydration:100%
+  outputHydration:80%
+  ---
+  type:simple
+  id:dough
+  stage:sourdough
+  W1800:50%
+  R2000:50%:sourdough
+  water:70%:sourdough
+  coriander:0.5%
+  salt:2%
+`;
+
+printToCsv(wwRyeFormula, 600, true, baseIngredients);
+// printToTerminal(wwRyeFormula, 600, true, baseIngredients);
