b1ff2812f8d1c7da4b57d78e48d979d4263e299a
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-05-22 16:18:00 +0100

    Added more known ingredients

--- projects/lib/src/lib/ingredients/ingredients.ts
+++ projects/lib/src/lib/ingredients/ingredients.ts
@@ -19,10 +19,14 @@ export const baseIngredients: Ingredient[] = [
   // Stage ingredient, allows to add dough development stages as ingredients
   { id: 'stage', type: IngredientType.stage },
 
+  // Combined ingredient, allows to add starters as ingredients
+  { id: 'starter', name: 'Starter', type: IngredientType.combined },
+
   // Wheat flour
   { id: 'W405', type: IngredientType.flour, name: 'Pastry flour', meta: 'Wheat' },
   { id: 'W550', type: IngredientType.flour, name: 'Plain flour', meta: 'Wheat' },
   { id: 'W700', type: IngredientType.flour, name: 'Bread flour', meta: 'Wheat' },
+  { id: 'W700X', type: IngredientType.flour, name: 'American Bread flour', meta: 'Wheat' },
   { id: 'W812', type: IngredientType.flour, meta: 'Wheat' },
   { id: 'W1050', type: IngredientType.flour, meta: 'Wheat' },
   { id: 'W1200', type: IngredientType.flour, meta: 'Wheat' },
@@ -50,6 +54,9 @@ export const baseIngredients: Ingredient[] = [
   { id: 'beer', type: IngredientType.hydration, name: 'Beer' },
   { id: 'cider', type: IngredientType.hydration, name: 'Cider' },
   { id: 'vinegar', type: IngredientType.hydration, name: 'Vinegar' },
+  { id: 'milk', type: IngredientType.hydration, name: 'Milk' },
+  { id: 'kefir', type: IngredientType.hydration, name: 'Kefir' },
+  { id: 'yogurt', type: IngredientType.hydration, name: 'Yogurt' },
 
   // Other
   { id: 'salt', type: IngredientType.other, name: 'Salt' },
@@ -58,13 +65,15 @@ export const baseIngredients: Ingredient[] = [
   { id: 'fennel', type: IngredientType.other, name: 'Fennel seeds' },
   { id: 'anise', type: IngredientType.other, name: 'Anise seeds' },
   { id: 'coriander', type: IngredientType.other, name: 'Coriander seeds' },
-  { id: 'caraway', type: IngredientType.other, name: 'Caraway seeds' },
-  { id: 'caraway', type: IngredientType.other, name: 'Caraway seeds' },
   { id: 'pumpkin-seeds', type: IngredientType.other, name: 'Pumpkin seeds' },
   { id: 'sunflower-seeds', type: IngredientType.other, name: 'Sunflower seeds' },
   { id: 'oil', type: IngredientType.other, name: 'Oil', meta: 'Any' },
   { id: 'veg-oil', type: IngredientType.other, name: 'Vegetable oil' },
   { id: 'olive-oil', type: IngredientType.other, name: 'Olive oil' },
   { id: 'butter', type: IngredientType.other, name: 'Butter' },
-  { id: 'sugar', type: IngredientType.other, name: 'Sugar' }
+  { id: 'sugar', type: IngredientType.other, name: 'Sugar' },
+  { id: 'treacle', type: IngredientType.other, name: 'Black treacle' },
+  { id: 'eggs', type: IngredientType.other, name: 'Eggs' },
+  { id: 'baking-powder', type: IngredientType.other, name: 'Baking powder' },
+  { id: 'cinnamon', type: IngredientType.other, name: 'Cinnamon' }
 ];
